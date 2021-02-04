4033f8789de99a345ec527a124f4877369e347a5
Achint Sandhu <sandhu@7theta.com>
2021-02-03 21:26:24 -0500

    Interval support

--- project.clj
+++ project.clj
@@ -8,17 +8,17 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any others, from this software.
 
-(defproject com.7theta/tempus "0.2.0"
+(defproject com.7theta/tempus "0.3.0"
   :description "Clojure(script) time library"
   :url "https://github.com/7theta/tempus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [inflections "0.13.2"]
                  [com.cognitect/transit-clj "1.0.324" :exclusions [com.fasterxml.jackson.core/jackson-core]]]
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :dependencies [[org.clojure/clojurescript "1.10.773"]
-                                  [org.clojure/tools.namespace "1.0.0"]]
+                                  [org.clojure/tools.namespace "1.1.0"]]
                    :source-paths ["dev"]}}
   :prep-tasks ["compile"]
   :scm {:name "git"
